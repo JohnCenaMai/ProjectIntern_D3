12191f6ddac0135837afa2f935a1da92f6c9a014
binhdanggioi <dangson06121998@gmail.com>
2021-01-11 17:13:31 +0700

    src/components/page/home/homepage.js

---
+++ fwb_fe/src/components/pages/home/homepage.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import styled from "styled-components";
+import {PageHeader,Tabs,Tooltip,  Form, Input, Radio, DatePicker} from 'antd';
+import 'antd/dist/antd.css';
+
+function HomePage() {
+    return(
+        <Fragment>
+            <div>111</div>
+        </Fragment>
+    );
+}
+
+export default HomePage;
