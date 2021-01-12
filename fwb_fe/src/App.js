607417c02dc050f0651a47215235a0bb1d820b5b
binhdanggioi <dangson06121998@gmail.com>
2021-01-12 14:34:50 +0700

    fix responsive

--- fwb_fe/src/App.js
+++ fwb_fe/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './components/authentication/login';
 import HomePage from './components/pages/home/homepage';
 
 function App() {
   return (
-    <HomePage />
+    <Login />
   );
 }
 
