139ae21b98ac07908d7ee6a044bf959df8861846
binhdanggioi <dangson06121998@gmail.com>
2021-01-12 11:25:15 +0700

    build layout home

--- fwb_fe/src/components/pages/home/homepage.js
+++ fwb_fe/src/components/pages/home/homepage.js
@@ -1,12 +1,26 @@
 import React, { Fragment } from 'react';
-import styled from "styled-components";
-import {PageHeader,Tabs,Tooltip,  Form, Input, Radio, DatePicker} from 'antd';
 import 'antd/dist/antd.css';
+import {Row,Col, Avatar} from 'antd';
+import Sidebar from '../../common/sidebar/sider';
+import Header from '../../common/header/header';
 
 function HomePage() {
+
+
     return(
         <Fragment>
-            <div>111</div>
+            <Row>
+                <Col span={18} push={6}>
+                    <Header />
+                    <Row>
+                    <Avatar size={{xs: 24,sm: 32, md: 40,lg: 64,xl: 80,xxl: 100,}}
+                    className="profile"/>
+                    </Row>
+                </Col>
+                <Col span={6} pull={18}>
+                    <Sidebar />
+                </Col>
+            </Row>
         </Fragment>
     );
 }
