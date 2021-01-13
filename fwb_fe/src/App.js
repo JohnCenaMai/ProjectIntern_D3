696c50bddf25c179d01476322931acab05868f78
binhdanggioi <dangson06121998@gmail.com>
2021-01-13 16:09:42 +0700

    defind routes redirect page

--- fwb_fe/src/App.js
+++ fwb_fe/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import Login from "./components/authentication/login";
 import HomePage from "./components/pages/home/homepage";
 import FeedPage from "./components/pages/feed/feedpage";
@@ -8,7 +9,22 @@ import Match from "./components/pages/match/match";
 
 function App() {
   return (
-    <Chat />
+      <Router>
+        <Switch>
+          <Route path="/find-near-you">
+            <HomePage />
+          </Route>
+          <Route path="/matches">
+            <Match />
+          </Route>
+          <Route path="/feeds">
+            <FeedPage />
+          </Route>
+          <Route path="/message">
+            <Chat />
+          </Route>
+        </Switch>
+      </Router>
   );
 }
 
