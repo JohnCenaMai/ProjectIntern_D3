c97a2f89ce357fa1a8ef10de4ae7d6a2c905818b
Nguyen Khac Lam <lamnk@domain.vnext.vn>
2021-01-13 10:11:45 +0700

    Add chat page to frontend

--- fwb_fe/src/components/common/recentChat/recentChat.js
+++ fwb_fe/src/components/common/recentChat/recentChat.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Avatar, Typography } from "antd";
+import "./recentChat.css";
 
 function RecentChatItem() {
   return (
     <div className="recentChatItem">
-      <Avatar size={64} />
+      <Avatar
+        size={64}
+        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+      />
       <div className="recentChatItem__infor">
         <Typography.Title level={4}>Last message</Typography.Title>
         <Typography.Text>Last message content</Typography.Text>
--- fwb_fe/src/components/pages/chat/chat.js
+++ fwb_fe/src/components/pages/chat/chat.js
@@ -1,16 +1,57 @@
 import React, { Fragment } from "react";
-import { Row, Col, Typography } from "antd";
+import { Row, Col, Typography, Input, Form, Avatar, Button } from "antd";
 import Sidebar from "../../common/sidebar/sider";
 import RecentChatItem from "./../../common/recentChat/recentChat";
-import { Input } from "antd";
+import { MessageOutlined, VideoCameraOutlined } from "@ant-design/icons";
 import "./chat.css";
+import ChatConversation from "../../common/chatConservation/chatConversation";
 
 function Chat() {
+  const handleSubmit = (values) => {
+    console.log("Submit", values.message);
+  };
+
   return (
     <Fragment>
       <Row>
         <Col span={13} push={5}>
-          <div className="chats"></div>
+          <div className="chats">
+            <Form
+              name="message"
+              onFinish={handleSubmit}
+              className="chats__form"
+            >
+              <Form.Item
+                name="message"
+                rules={[{ required: true, message: "Please type something!" }]}
+              >
+                <Input
+                  prefix={<MessageOutlined />}
+                  size="large"
+                  placeholder="Type something..."
+                />
+              </Form.Item>
+            </Form>
+
+            <div className="chats__header">
+              <div className="chats__headerLeft">
+                <Avatar size={64} />
+                <div className="chat__header--infor">
+                  <Typography.Title level={3}>Nguyen Khac Lam</Typography.Title>
+                  <Typography.Text>Last send: 10 hours ago</Typography.Text>
+                </div>
+              </div>
+
+              <Button type="link">
+                <VideoCameraOutlined
+                  style={{ fontSize: "32px" }}
+                  twoToneColor="#eb2f96"
+                />
+              </Button>
+            </div>
+
+            <ChatConversation />
+          </div>
         </Col>
         <Col span={6} push={5}>
           <div className="chats__sidebar">
@@ -23,6 +64,13 @@ function Chat() {
             <div className="recentChats">
               <Typography.Title level={5}>Recent chats</Typography.Title>
 
+              <RecentChatItem />
+              <RecentChatItem />
+              <RecentChatItem />
+              <RecentChatItem />
+              <RecentChatItem />
+              <RecentChatItem />
+              <RecentChatItem />
               <RecentChatItem />
             </div>
           </div>
