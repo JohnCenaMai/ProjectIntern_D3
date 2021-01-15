52dd780d16f0711fc3378bb6fcf840f98dcfe77d
Nguyen Khac Lam <lamnk@domain.vnext.vn>
2021-01-15 15:30:50 +0700

    Done add and delete from frontend to backend

--- fwb_fe/src/components/common/feedItem/feedItem.js
+++ fwb_fe/src/components/common/feedItem/feedItem.js
@@ -1,61 +1,108 @@
 import React, { Fragment } from "react";
 import "./feedItem.css";
 import { Avatar, Image, Typography } from "antd";
-import { Menu, Dropdown,Row,Col } from "antd";
-import { MoreOutlined, LikeOutlined, CommentOutlined } from "@ant-design/icons";
+import { Menu, Dropdown, Row, Col } from "antd";
+import {
+  MoreOutlined,
+  LikeOutlined,
+  LikeFilled,
+  CommentOutlined,
+} from "@ant-design/icons";
+import ReactTimeAgo from "react-time-ago";
 
-const dropdownMenu = (
-  <Menu>
-    <Menu.Item key="0">Edit post</Menu.Item>
-    <Menu.Item key="1">Delete Post</Menu.Item>
-  </Menu>
-);
+// Redux
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { likePost, deletePost } from "./../../../redux/actions/post";
 
-function FeedItem() {
+function FeedItem({ post, user, likePost, deletePost }) {
+  const dropdownMenu = (
+    <Menu onClick={(e) => handleMenuClick(e)}>
+      <Menu.Item key="0">Edit post</Menu.Item>
+      <Menu.Item key="1">Delete Post</Menu.Item>
+    </Menu>
+  );
+
+  const handleLike = () => {
+    likePost(post.id);
+  };
+
+  const handleMenuClick = (e) => {
+    switch (e.key) {
+      case "0":
+        break;
+      case "1":
+        deletePost(post.id);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <Row>
       <Col span={24} push={3}>
-          <div className="feedItem">
+        <div className="feedItem">
           <div className="feedItem__header">
             <div className="feedItem__header--infor">
-              <Avatar src="" />
+              <Avatar
+                src={
+                  post.userImageUrl &&
+                  `http://localhost:5000/${post.userImageUrl}`
+                }
+              />
               <div className="feedItem__header--inforText">
                 <p>
-                  <span>Nguyen Khac lam</span> just created a post
+                  <span>{post.username}</span> just created a post
                 </p>
-                <small>5 hours ago</small>
+                <small>
+                  <ReactTimeAgo date={post.created_at} locale="en-US" />
+                </small>
               </div>
             </div>
             <div className="feedItem__header--action">
-              <Dropdown
-                overlay={dropdownMenu}
-                trigger={["click"]}
-                placement="bottomRight"
-              >
-                <MoreOutlined style={{ fontSize: "32px" }} />
-              </Dropdown>
+              {user.data.username !== post.username ? (
+                ""
+              ) : (
+                <Dropdown
+                  overlay={dropdownMenu}
+                  trigger={["click"]}
+                  placement="bottomRight"
+                >
+                  <MoreOutlined style={{ fontSize: "32px" }} />
+                </Dropdown>
+              )}
             </div>
           </div>
           <div className="feedItem__content">
             <Typography.Paragraph
               ellipsis={{ rows: 2, expandable: true, symbol: "more" }}
             >
-              I've created a poll to ask all of you what you would like to see more
-              in my upcoming videos! It will only take a couple of seconds...thanks
-              to all in advance!! Sorry everyone, but from now on, I will only be
-              able to edit and upload one design tutorial per month. This happens
-              because I'm having a lot on my plate right now and recording and
-              editing the tutorials requiere a lot of attention.
+              {post.content}
             </Typography.Paragraph>
           </div>
           <div className="feedItem__image">
-            <Image src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
+            {post.imageUrl ? (
+              <Image src={`http://localhost:5000/images/${post.imageUrl}`} />
+            ) : (
+              ""
+            )}
           </div>
           <div className="feedItem__footer">
             <div className="feedItem__footer--infor">
-              <LikeOutlined style={{ fontSize: "24px", marginRight: "0.5rem" }} />
-              <Typography.Text>15 likes</Typography.Text>
+              {post.like.includes(user.data.email) ? (
+                <LikeFilled
+                  style={{ fontSize: "24px", marginRight: "0.5rem" }}
+                />
+              ) : (
+                <LikeOutlined
+                  style={{ fontSize: "24px", marginRight: "0.5rem" }}
+                  onClick={handleLike}
+                />
+              )}
+              <Typography.Text>
+                {post.like ? post.like.length : 0} likes
+              </Typography.Text>
             </div>
             <div className="feedItem__footer--infor">
               <CommentOutlined
@@ -70,4 +117,13 @@ function FeedItem() {
   );
 }
 
-export default FeedItem;
+FeedItem.prototype = {
+  likePost: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.auth.user,
+});
+
+export default connect(mapStateToProps, { likePost, deletePost })(FeedItem);
